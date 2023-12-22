f9a1d764b7c5e2ddc272861308c5e86f5aa30b6c
dillon-bb <137834102+dillon-bb@users.noreply.github.com>
2023-12-22 11:46:50 -0700

    checkpoint

--- api/src/main/java/com/it_inventory/api/Static/StaticAssetController.java
+++ api/src/main/java/com/it_inventory/api/Static/StaticAssetController.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import java.util.List; // Added import for List
 
 @Controller
@@ -30,6 +32,5 @@ public class StaticAssetController {
             return "error_fragment"; // an error fragment view
         }
     }
-
 
 }
--- api/src/main/java/com/it_inventory/api/Static/StaticController.java
+++ api/src/main/java/com/it_inventory/api/Static/StaticController.java
@@ -2,17 +2,13 @@ package com.it_inventory.api.Static;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
 @Controller
 public class StaticController {
 
     @RequestMapping("/")
     public String index() {
-        return "index"; // This refers to 'index.html' in 'src/main/resources/templates'
-    }
-
-    @RequestMapping("/asset-list")
-    public String assetList() {
-        return "asset-list"; // This refers to 'asset-list' in src/main/resources/templates
+        return "asset-list"; // This refers to 'index.html' in 'src/main/resources/templates'
     }
 
     @RequestMapping("/inventory-list")
