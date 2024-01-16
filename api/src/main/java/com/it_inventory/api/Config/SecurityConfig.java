b854a4a0bd2cca463484530048de06670dc70133
dillon-bb <137834102+dillon-bb@users.noreply.github.com>
2024-01-16 14:49:22 -0700

    working backend for azure auth

---
+++ api/src/main/java/com/it_inventory/api/Config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.it_inventory.api.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authz -> authz.anyRequest().authenticated())
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.decoder(jwtDecoder())));
+        return http.build();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        String jwkSetUri = "https://login.microsoftonline.com/95b4977b-2e19-4499-8928-882caf67f448/discovery/v2.0/keys";
+        return NimbusJwtDecoder.withJwkSetUri(jwkSetUri).build();
+    }
+}
