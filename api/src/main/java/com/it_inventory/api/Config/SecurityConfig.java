2d9aab6fe2e43fbdd5d95f6afaa5021543f37944
Joey McRae <joey.mcrae@3zbrands.com>
2024-02-26 10:39:46 -0700

    changed CORS, is now part of AUTH

--- api/src/main/java/com/it_inventory/api/Config/SecurityConfig.java
+++ api/src/main/java/com/it_inventory/api/Config/SecurityConfig.java
@@ -3,9 +3,17 @@ package com.it_inventory.api.Config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.security.config.Customizer;
+
+
+import java.util.Arrays;
 
 @Configuration
 public class SecurityConfig {
@@ -13,6 +21,8 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                .cors(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authz -> authz
                         .requestMatchers("/").permitAll()
                         .anyRequest().authenticated())
@@ -24,5 +34,17 @@ public class SecurityConfig {
     public JwtDecoder jwtDecoder() {
         String jwkSetUri = "https://login.microsoftonline.com/95b4977b-2e19-4499-8928-882caf67f448/discovery/v2.0/keys";
         return NimbusJwtDecoder.withJwkSetUri(jwkSetUri).build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:8081", "https://inventory.3zms.com")); // Update as needed
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
